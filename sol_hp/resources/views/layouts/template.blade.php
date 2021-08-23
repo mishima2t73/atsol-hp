504b84c9aed7ce575f4fbc98d3ef1a93ae8c82bf
mishima2t73 <shima.k7646@gmail.com>
2021-08-23 17:21:10 +0900

    template-top

--- sol_hp/resources/views/layouts/template.blade.php
+++ sol_hp/resources/views/layouts/template.blade.php
@@ -5,11 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
-    <title>@sol</title>
+    <title id = "title">@sol</title>
 </head>
 <body class="bg-light">
 <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-white ">
+    <nav class="navbar navbar-expand-lg navbar-light bg-white shadow">
         <a class="navbar-brand" href="#">
             <img src="{{asset('t_head_logo.png')}}" alt="">
         </a>
@@ -17,7 +17,7 @@
           <span class="navbar-toggler-icon"></span>
         </button>
       
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse " id="navbarSupportedContent">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item active">
               <a class="nav-link" href="#">事業案内 <span class="sr-only">(current)</span></a>
@@ -31,6 +31,9 @@
             <li class="nav-item active">
             <a class="nav-link" href="#">採用情報<span class="sr-only">(current)</span></a>
             </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="#">お問い合わせ<span class="sr-only">(current)</span></a>
+              </li>
           </ul>
 
         </div>
@@ -39,6 +42,38 @@
     @section('content')
 
     @show
+    <footer class="bg-white mt-5">
+      <div>
+        <div class="row m-2">
+          <div class="col m-2 p-2 bg-secondary rounded text-light">TELL</div>
+          <div class="col m-2 p-2 bg-secondary rounded text-light">採用情報-Recruit</div>
+          <div class="col m-2 p-2 bg-secondary rounded text-light">個人情報保護方針</div>
+        </div>
+        <div class ="bg-secondary m-3">
+          <div class="row m-2 align-items-center">
+            <div class="col "><img src="{{asset('t_head_logo.png')}}" alt=""></div>
+            <div class="col bg-light m-3 p-2">
+              【大阪本社】<br>
+              〒542-0061<br>
+              大阪市中央区安堂寺町2-1-10／ベルデ谷町2階
+              <br>
+              T E L　 06-7506-9304<br>
+              F A X 　06-7506-9305</div>
+            <div class="col bg-light m-3 p-2">
+              【東京支社】<br>
+                〒113-0033<br>
+                東京都文京区本郷3丁目9-11 平原ビル202号室<br>
+                T E L 　03-6240-0175<br>
+                F A X　 050-3737-1951<br>
+            </div>
+          </div>
+        </div>
+        <div class="text-center mb-3 ">
+          <a href="#title" class="text-dark border-bottom border-dark rounded p-1">Page Top</a>
+        </div>
+        
+      </div>
+    </footer>
 </div>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
--- sol_hp/resources/views/top.blade.php
+++ sol_hp/resources/views/top.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.template')
 
 @section('content')
-   
+<div class=""style="margin-bottm:100px;">
+   <div class="jumbotron ">
+      <div class="container">
+         <div class="align-item-center"><img style = "width:100%;height:80%;"src="{{asset('top.jpg')}}" alt=""></div>
+      </div>
+   </div>
+</div>
+
+<div>
+ 
+</div>
+   <h1>test</h1>
 @endsection
