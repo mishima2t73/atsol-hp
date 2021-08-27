517e41b24e24113180952bb9ca4cd9b752871c40
hi-roya <huroya0712@gmail.com>
2021-08-27 12:05:00 +0900

    pullします。companyはほぼ完成(のはず)

--- sol_hp/resources/views/company.blade.php
+++ sol_hp/resources/views/company.blade.php
@@ -1,5 +1,64 @@
 @extends('layouts.template')
 
 @section('content')
-   
+   <div id="company"class="bg-light">
+       <h1 class="text-center">company</h1>
+       <h2 class="mx-auto">会社概要</h2>
+       <tbody>
+           <tr>
+               <td width="190" valign="top"class="comCeiL">商号</td>
+               <td width="470" valign="top"class="comCelR">株式会社アットマークソリューション</td>
+</tr>
+<tr>
+    <td valign="top"class="comCelL">所在地</td>
+    <td valign="top"class="comCelR">
+        "本社:"
+        <br>
+        "大阪府大阪市中央区安堂寺町2-1-10 ベルテ谷町2F(202)"
+        <br>
+        <br>
+        "東京支社:"
+        <br>
+        "東京都文京区本郷3丁目9-11 平原ビル202号室"
+</td>
+</tr>
+<tr>
+    <td valign="top" class="comCelL">創業・設立</td>
+    <td valign="top" class="comCelR">2005年7月</td>
+</tr>
+<tr>
+    <td valign="top" class="comCelL">代表取締役</td>
+    <td valign="top" class="comCelR">増田勝人</td>
+</tr>
+<tr>
+    <td valign="top" class="comCelL">役員</td>
+    <td valign="top" class="comCelR">谷口耕久</td>
+</tr>
+<td valign="top" class="comCelL">事業内容</td>
+<td valign="top" class="comCelR">Webシステム開発、ECサイト企画運営
+    Webコンサルティング、基盤システム設計・構築、システム運用保守、プログラム開発、基盤システム技術の教育</td>
+</tr>
+<tr>
+    <td valign="top"class="comCelL">従業員数</td>
+    <td valign="top"class="comCelR">n人</td>
+</tr>
+<tr>
+    <td valign="top"class="comCelL">主要取引先</td>
+    <td valign="top"class="comCelR">〇〇株式会社、株式会社□□</td>
+</tr>
+<tr>
+<td valign="top" class="comCelLa">本社アクセスマップ</td>
+<td valign="top" class="comCelRa">&nbsp;</td>
+</tr>
+<td colspan="2" valign="top" class="comCelLb"></td>
+<iframe src="https://www.google.com/maps/embed?key=AIzaSyDV01RT0PITf-2ptIdKiqKZ1XX-xWSOXc8&amp;pb=!1m18!1m12!1m3!1d3281.1307836432657!2d135.51622475000005!3d34.676648549999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000e7375d79697b%3A0xac126043cb490ea3!2z44OZ44Or44OH6LC355S6!5e0!3m2!1sja!2sjp!4v1398126455383" width="622" height="292" frameborder="0" style="border:0"></iframe>
+    <div class="pageTop">
+        <div class="pageTopbotton">
+            <a href="#topPosition">
+                <div class="pageTopbotton">ページトップ</div>
+</a>
+</div>
+</div>
+</tbody>
+
 @endsection
